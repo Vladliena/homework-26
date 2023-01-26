1bb221b99200401dd75b4c5c12ad06f30bf1979c
vladliena <sotnikovavladlena@gmail.com>
2023-01-26 16:49:22 +0200

    firts commit

--- js/new_div_script.js
+++ js/new_div_script.js
@@ -1,17 +1,16 @@
-
-const text_input = document.getElementsByTagName('input')[0];
+const mainText = document.getElementsByTagName('h1')[0]
 const mainContainer = document.getElementsByClassName('container') [0]
 const divFromInput = document.createElement('div');
 divFromInput.className = 'new_div'
 divFromInput.innerHTML = `<h3 class="title">New Div</h3>`
 
-    text_input.addEventListener('mouseover', newDiv);
-    text_input.addEventListener('mouseout', deleteNewDiv);
+mainText.addEventListener('mouseover', newDiv);
+mainText.addEventListener('mouseout', deleteNewDiv);
 
-function newDiv (){
+function newDiv() {
     mainContainer.appendChild(divFromInput)
 }
 
-function deleteNewDiv(){
+function deleteNewDiv() {
     mainContainer.removeChild(divFromInput)
 }
